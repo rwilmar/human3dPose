f3d1a472bfad74e4a0326758b8e62b30be9fa6a7
Wilmar Rodriguez <rwilmar@gmail.com>
2020-03-02 21:27:23 +0100

    pose tests update

--- app.py
+++ app.py
@@ -2,7 +2,15 @@ import argparse
 import cv2
 import numpy as np
 
+from inference import Network
+from model_handlers import handle_output
+from image_handlers import preprocessing, create_output_image
+
 #source /opt/intel/openvino/bin/setupvars.sh            ##remember##
+
+_MODEL_BOXES="./models/person-detection-retail-0013.xml" # more available with different inputs !!!
+_MODEL_MULTIPOSE="./models/human-pose-estimation-0001.xml"
+_MODEL_SINGLEPOSE="./models/000.xml" 
 
 def get_args():
     '''
@@ -16,7 +24,6 @@ def get_args():
     c_desc = "CPU extension file location, if applicable"
     d_desc = "Device, if not CPU (GPU, FPGA, MYRIAD)"
     i_desc = "The location of the input image"
-    m_desc = "The location of the model XML file"
     t_desc = "The type of model: POSE, TEXT or CAR_META or PERSON_DET"
 
     # -- Add required and optional groups
@@ -26,17 +33,74 @@ def get_args():
 
     # -- Create the arguments
     required.add_argument("-i", help=i_desc, required=True)
-    required.add_argument("-m", help=m_desc, required=True)
-    required.add_argument("-t", help=t_desc, required=True)
+    required.add_argument("-t", help=t_desc, default="PERSON_DET")
     optional.add_argument("-c", help=c_desc, default=None)
     optional.add_argument("-d", help=d_desc, default="CPU")
     args = parser.parse_args()
+    args.m=get_model(args.t)
 
     return args
 
+def get_model(model_type):
+    '''
+    Returns the related model, based on the model_type required.
+    '''
+    if model_type == "POSE":
+        return _MODEL_MULTIPOSE
+    elif model_type == "PERSON_DET":
+        return _MODEL_BOXES
+    else:
+        return _MODEL_MULTIPOSE
+
+def perform_inference(args):
+    '''
+    Performs inference #1 on an input image, given a model.
+    '''
+    # Create a Network for using the Inference Engine
+    inference_network = Network()
+    # Load the model in the network, and obtain its input shape
+    n, c, h, w = inference_network.load_model(args.m, args.d, args.c)
+
+    # Read the input image
+    image = cv2.imread(args.i)
+
+    ### TODO: Preprocess the input image
+    preprocessed_image = preprocessing(image, h, w)
+    #preprocessed_image = None
+
+    # Perform synchronous inference on the image
+    inference_network.sync_inference(preprocessed_image)
+
+    # Obtain the output of the inference request
+    output = inference_network.extract_output()
+
+    ### TODO: Handle the output of the network, based on args.t
+    ### Note: This will require using `handle_output` to get the correct
+    ###       function, and then feeding the output to that function.
+    #processed_output = None
+    processed_output = handle_output(args.t)(output,image.shape)
+
+    # Create an output image based on network
+    try:
+        output_image = create_output_image(args.t, image, processed_output)
+        print("Processing succeded :)")
+    except:
+        output_image = image
+        print("Image not processed :(")
+        
+    #output_image = create_output_image(args.t, image, processed_output)
+
+    # Save down the resulting image
+    cv2.imwrite("./images/{}-output.png".format(args.t), output_image)
+
+
+
 def main():
     args = get_args()
+
     print("test")
+
+    perform_inference(args)
 
 
 if __name__ == "__main__":
--- image_handlers.py
+++ image_handlers.py
@@ -47,3 +47,56 @@ def draw_boxes(frame, result, ct, width, height):
             print(xmin, ymin, xmax, ymax)
             cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (0, 153, 255), 2)
     return frame
+
+def create_output_image(model_type, image, output):
+    '''
+    Using the model type, input image, and processed output,
+    creates an output image showing the result of inference.
+    '''
+    [h, w, c] = image.shape
+    if model_type == "POSE":
+        # Remove final part of output not used for heatmaps
+        output = output[:-1]
+        # Get only pose detections above 0.5 confidence, set to 255
+        for c in range(len(output)):
+            output[c] = np.where(output[c]>0.4, 255, 0)
+        # Sum along the "class" axis
+        output = np.sum(output, axis=0)
+        # Get semantic mask
+        pose_mask = get_mask(output)
+        # Combine with original image
+        image = image + pose_mask
+        return image
+    elif model_type == "TEXT":
+        # Get only text detections above 0.5 confidence, set to 255
+        output = np.where(output[1]>0.5, 255, 0)
+        # Get semantic mask
+        text_mask = get_mask(output)
+        # Add the mask to the image
+        image = image + text_mask
+        return image
+
+    elif model_type == "CAR_META":
+        # Get the color and car type from their lists
+        color = CAR_COLORS[output[0]]
+        car_type = CAR_TYPES[output[1]]
+        # Scale the output text by the image shape
+        scaler = max(int(image.shape[0] / 1000), 1)
+        # Write the text of color and type onto the image
+        image = cv2.putText(image, 
+            "Color: {}, Type: {}".format(color, car_type), 
+            (50 * scaler, 100 * scaler), cv2.FONT_HERSHEY_SIMPLEX, 
+            2 * scaler, (255, 255, 255), 3 * scaler)
+        return image
+
+    elif model_type == "PERSON_DET":
+        print("returned")
+        # Get only text detections above 0.5 confidence, set to 255
+        #output = np.where(output[1][2]>0.5, output, 0)
+        nPers, perVector = output.shape
+        print("modelo de personas: ", nPers)
+        
+        return draw_boxes(image, output, 0.3, w, h)
+    else:
+        print("Unknown model type, unable to create output image.")
+        return image
